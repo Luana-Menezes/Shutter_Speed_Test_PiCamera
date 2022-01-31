6ad46c74ca0a7c65cb28e77b3726d3d5f689d0cf
Luana Menezes <luana.menezes.lm@gmail.com>
2022-01-31 11:22:42 -0300

    task_shutter condition

--- task_shutter.py
+++ task_shutter.py
@@ -2,7 +2,8 @@ from time import sleep, time
 from picamera import PiCamera
 from datetime import datetime
 
-NIGHT_TIME_START='18' 
+DAY_TIME_START=7
+NIGHT_TIME_START=17 
 SHUTTER_SPEED_NIGHT=3000
 
 camera = PiCamera()
@@ -11,7 +12,11 @@ camera.rotation = -90
 camera.framerate = 30
 exposure_mode = 'sports'
 
-if (datetime.now().strftime('%H') == NIGHT_TIME_START):
+hour = int(datetime.now().strftime('%H'))
+
+if (hour >= NIGHT_TIME_START and hour <= DAY_TIME_START):
 	camera.shutter_speed = SHUTTER_SPEED_NIGHT
+else
+	camera.shutter_speed = 0
 
 camera.capture('teste.jpg')
